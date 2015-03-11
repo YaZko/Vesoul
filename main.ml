2af742b2c98f400f87dfafdfe9b8998ad5e9d938
Yannick <Rainette@rainette.irisa.fr>
2015-03-11 19:26:52 +0100

    coucou

--- main.ml
+++ main.ml
@@ -1,4 +1,13 @@
 
+let read_lines name : string list =
+  let ic = open_in name in
+  let try_read () =
+    try Some (input_line ic) with End_of_file -> None in
+  let rec loop acc = match try_read () with
+    | Some s -> loop (s :: acc)
+    | None -> close_in ic; List.rev acc in
+  loop []
+
 
 let load_file f =
   let ic = open_in f in
